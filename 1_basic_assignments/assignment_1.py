cec01a61864df701fd38a1a62a29c95e664b2daa
swapnil <swapnil@HP.example.com>
2021-03-20 15:53:56 +0530

    basic 1_assignment

---
+++ 1_basic_assignments/assignment_1.py
@@ -0,0 +1,92 @@
+'''
+1. In the below elements which of them are values or an expression? eg:- values can be
+integer or string and expressions will be mathematical operators.
+*
+&#39;hello&#39;
+-87.8
+-
+/
++
+6
+'''
+
+
+
+* --> expression
+hello --> values 
+& --> expression
+-87.8 --> values
+- --> expression
+/ --> expression
++ --> expression
+6 --> values
+
+
+'''
+2. What is the difference between string and variable?
+'''
+string is stored with the help of variable.
+e.g
+name='swapnil'
+here name is variable and swapnil is string.
+
+'''
+3. Describe three different data types.
+
+'''
+integer-->it includes values which are whole numbers.these are discrete values.float e.g 5
+float-->this includes values which are continuous in nature. e.g 5.5
+string-->includes Sequence of characters . e,g-"swapnil"
+
+'''
+4. What is an expression made up of? What do all expressions do?
+'''
+expression consists of values,variables,strings,operators and many other components.
+all combined they generate a result of particular operation user want to perform.
+
+'''
+5. This assignment statements, like spam = 10. What is the difference between an
+expression and a statement?
+'''
+when we perform some expression,we get come result.
+but when we execute the assignment statements,,we just assign the value to some variable.which does not  return any result.
+
+'''
+6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+'''
+22
+
+
+
+'''
+7. What should the values of the following two terms be?
+'spam' +'spamspam'
+'spam'* 3
+'''
+spamspamspam
+spamspamspam
+
+'''
+8. Why is eggs a valid variable name while 100 is invalid?
+'''
+we cant assign integer as a variable name.so 100 is invalid.
+
+'''
+9. What three functions can be used to get the integer, floating-point number, or string
+version of a value?
+'''
+for integer --> int
+for float --> float
+for string --> string
+'''
+10. Why does this expression cause an error? How can you fix it?
+'I have eaten'+ 99 + 'burritos'
+'''
+we cannot add string and integer.
+we need to convert that 99 into string.so that we can add them all.
+
+solution-->
+'I have eaten'+ str(99) + 'burritos'
+
